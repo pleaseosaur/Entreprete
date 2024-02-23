9635b93fc15d51b3e5ce3f350ea0d80c9ea8dc80
clearwm <clearwm@wwu.edu>
2024-02-22 16:46:54 -0800

    added text components to match figma

--- components/Text.js
+++ components/Text.js
@@ -4,16 +4,29 @@ const Text = ({ style, ...rest }) => {
 //   const { colors } = useThemeColors();
 
   return (
-      <DefaultText/>
-    // <DefaultText
-    //   style={[
-    //     { color: colors.text, fontFamily: 'SpaceMono-Regular', fontSize: 16 },
-    //     style,
-    //   ]}
-    //   {...rest}
-    // />
+    <DefaultText
+      style={[
+        { color: '#48474B', fontFamily: 'NotoSans-Regular', fontSize: 14 },
+        style,
+      ]}
+      {...rest}
+    />
   );
 };
+
+const ListText = ({ style, ...rest }) => {
+    //   const { colors } = useThemeColors();
+    
+      return (
+        <DefaultText
+          style={[
+            { color: '#48474B', fontFamily: 'NotoSans-Regular', fontSize: 14 , lineHeight: 26},
+            style,
+          ]}
+          {...rest}
+        />
+      );
+    };
 
 const ButtonText = ({ style, ...rest }) => {
 //   const { colors } = useThemeColors();
@@ -43,5 +56,33 @@ const ScreenTitleText = ({ style, ...rest }) => {
       );
     };
 
-export { Text, ButtonText, ScreenTitleText };
+const HomeTitleText = ({ style, ...rest }) => {
+    //   const { colors } = useThemeColors();
+    
+        return (
+        <DefaultText
+            style={[
+            { color: '#48474B', fontFamily: 'Nunito-Black', fontSize: 48 },
+            style,
+            ]}
+            {...rest}
+        />
+        );
+    };
+
+const HeaderText = ({ style, ...rest }) => {
+    //   const { colors } = useThemeColors();
+    
+        return (
+        <DefaultText
+            style={[
+            { color: '#48474B', fontFamily: 'Nunito-Bold', fontSize: 20 },
+            style,
+            ]}
+            {...rest}
+        />
+        );
+    };
+
+export { Text, ButtonText, ScreenTitleText, ListText, HomeTitleText, HeaderText };
 export default Text;
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
+import { HomeTitleText } from '../../components/Text';
 import style from './style';
 
 const Home = () => {
   return (
     <SafeAreaView style={style.homeContainer}>
       <View style={style.homeTitleContainer}>
-        <Text style={style.homeTitle}>Entreprête</Text>
+        <HomeTitleText>Entreprête</HomeTitleText>
       </View>
       <View style={style.homeIconContainer}>
         <View>
