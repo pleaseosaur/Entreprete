1dc25fd308d34fc57a6dc10ae3d516dd57431042
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2024-03-08 20:16:06 -0800

    undoing merge overwrites

--- components/AddButton/AddButton.js
+++ components/AddButton/AddButton.js
@@ -2,7 +2,7 @@ import React, {useState, useRef} from 'react';
 import {View, ScrollView, Text} from 'react-native';
 import {Overlay} from 'react-native-elements';
 import {SquareButton} from '../Button/Button';
-import {PlusCircle} from '../Icons/Icons';
+import {EditMessageBook, Link, PlusCircle, Vuesax} from '../Icons/Icons';
 import {MinusCircle} from '../Icons/Icons';
 import style from './style';
 
@@ -10,9 +10,7 @@ const AddButton = () => {
     const [visible, setVisible] = useState(false);
     const ref = useRef(null);
 
-    const addRecipe = () => {
-
-    };
+    const addRecipe = () => {};
 
     const toggleOverlay = () => {
         setVisible(!visible);
@@ -20,16 +18,28 @@ const AddButton = () => {
 
     return (
         <View>
-            <SquareButton handler={toggleOverlay} icon={<PlusCircle />}></SquareButton>
-            <Overlay overlayStyle={style.overlayContainer} backdropStyle={style.overlayBackdrop} isVisible={visible}>
+            <SquareButton
+                handler={toggleOverlay}
+                icon={<PlusCircle />}></SquareButton>
+            <Overlay
+                overlayStyle={style.overlayContainer}
+                backdropStyle={style.overlayBackdrop}
+                isVisible={visible}>
                 <View style={style.buttonContainer}>
                     <View style={style.row}>
-                        <SquareButton style={[style.button]} icon={<PlusCircle />}></SquareButton>
-                        <SquareButton style={[style.button]} icon={<PlusCircle />}></SquareButton>
+                        <SquareButton
+                            style={[style.button]}
+                            icon={<Vuesax />}></SquareButton>
+                        <SquareButton style={[style.button]} icon={<Link />}></SquareButton>
                     </View>
                     <View style={style.row}>
-                        <SquareButton style={[style.button, style.primaryButton]} handler={toggleOverlay} icon={<MinusCircle />}></SquareButton>
-                        <SquareButton style={[style.button]} icon={<PlusCircle />}></SquareButton>
+                        <SquareButton
+                            style={[style.button, style.primaryButton]}
+                            handler={toggleOverlay}
+                            icon={<MinusCircle />}></SquareButton>
+                        <SquareButton
+                            style={[style.button]}
+                            icon={<EditMessageBook />}></SquareButton>
                     </View>
                 </View>
                 <View style={style.buffer}></View>
