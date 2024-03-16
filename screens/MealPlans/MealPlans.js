ad1fb643acb5fba8f421ef4f1ae2c67391807202
benjaminlin808 <benjaminlin808@gmail.com>
2024-03-16 15:18:32 -0700

    fix mealplan map issue

--- screens/MealPlans/MealPlans.js
+++ screens/MealPlans/MealPlans.js
@@ -1,85 +1,90 @@
 import React, {useState} from 'react';
-import { View, ScrollView } from 'react-native';
-import BaseScreen from "../BaseScreen/BaseScreen";
-import {SquareButton} from "../../components/Button/Button";
-import {PlusCircle, Home} from "../../components/Icons/Icons";
+import {View, ScrollView} from 'react-native';
+import BaseScreen from '../BaseScreen/BaseScreen';
+import {SquareButton} from '../../components/Button/Button';
+import {PlusCircle, Home} from '../../components/Icons/Icons';
 import ListItem from '../../components/ListItem/ListItem';
 import style from './style';
 import testData from '../../mockServer/db.json';
 import SearchBar from '../../components/SearchBar/SearchBar';
-import { MealPlanSearch } from '../../mockServer/functionality/searchFunctions';
-import MealPlanItem from "../../components/MealPlanItem/MealPlanItem";
+import {MealPlanSearch} from '../../mockServer/functionality/searchFunctions';
+import MealPlanItem from '../../components/MealPlanItem/MealPlanItem';
 
 const MealPlans = ({navigation, mealPlanName}) => {
-    const [plans, setPlans] = useState(testData.mealPlans);
+  const [plans, setPlans] = useState(testData.mealplans);
 
-    const goBack = () => {
-        navigation.goBack();
-    };
+  const goBack = () => {
+    navigation.goBack();
+  };
 
-    const goHome = () => {
-        navigation.navigate('Home');
-    };
+  const goHome = () => {
+    navigation.navigate('Home');
+  };
 
-    const selectMealPlan = (plan) => {
-        navigation.navigate('MealPlan', {mealPlan: plan})
-    };
+  const selectMealPlan = plan => {
+    navigation.navigate('MealPlan', {mealPlan: plan});
+  };
 
-    const handleSearch = async (text) => {
-        if (text === '') {
-          setPlans(testData.mealPlans);
-          return;
-        }
-        const searchResult = await MealPlanSearch(text);
-        setPlans(searchResult);
-    };
-
-    const addPlan = () => {
-        navigation.navigate('AddMealPlan')
+  const handleSearch = async text => {
+    if (text === '') {
+      setPlans(testData.mealPlans);
+      return;
     }
+    const searchResult = await MealPlanSearch(text);
+    setPlans(searchResult);
+  };
 
-    const deletePlan = index => {
-        setPlans(prev => {
-            const newPlans = prev.filter((_, i) => i !== index);
-            return newPlans;
-        });
-    };
+  const addPlan = () => {
+    navigation.navigate('AddMealPlan');
+  };
 
-    return (
-        <BaseScreen
-            title="Meal Plans"
-            canGoBack={true}
-            canEdit={false}
-            goBack = {goBack}>
-            <ScrollView style={style.scrollContainer}>
-                {plans.map((plan, index) => {
-                    return (
-                        <ListItem
-                        name={mealPlanName ? mealPlanName : plan.name}
-                        id={plan.id}
-                        key={index}
-                        onPress={() => selectMealPlan(plan)}
-                        swipeHandler={() => deletePlan(index)}
-                        />
-                    );
-                })}
-            </ScrollView>
-            <View style={style.plansSearchContainer}>
-                <View style={style.buttonsContainer}>
-                    <View>
-                        <SquareButton icon={<PlusCircle />} handler={addPlan}></SquareButton>
-                    </View>
-                    <View>
-                        <SquareButton handler={goHome} icon={<Home />}></SquareButton>
-                    </View>
-                    <View style={style.buttonPlaceHolder}></View>
-                </View>
-                <View>
-                    <SearchBar placeholderText={"Search meal plans"} handleSearch={handleSearch} />
-                </View>
-            </View>
-        </BaseScreen>
-    );
-}
+  const deletePlan = index => {
+    setPlans(prev => {
+      const newPlans = prev.filter((_, i) => i !== index);
+      return newPlans;
+    });
+  };
+
+  return (
+    <BaseScreen
+      title="Meal Plans"
+      canGoBack={true}
+      canEdit={false}
+      goBack={goBack}>
+      <ScrollView style={style.scrollContainer}>
+        {plans.map((plan, index) => {
+          return (
+            <ListItem
+              name={mealPlanName ? mealPlanName : plan.name}
+              id={plan.id}
+              key={index}
+              onPress={() => selectMealPlan(plan)}
+              swipeHandler={() => deletePlan(index)}
+            />
+          );
+        })}
+      </ScrollView>
+      <View style={style.plansSearchContainer}>
+        <View style={style.buttonsContainer}>
+          <View>
+            <SquareButton
+              icon={<PlusCircle />}
+              handler={addPlan}></SquareButton>
+          </View>
+          <View>
+            <SquareButton handler={goHome} icon={<Home />}></SquareButton>
+          </View>
+          <View style={style.buttonPlaceHolder}></View>
+        </View>
+        <View>
+          <SearchBar
+            placeholderText={'Search meal plans'}
+            handleSearch={handleSearch}
+          />
+        </View>
+      </View>
+    </BaseScreen>
+  );
+};
 
 export default MealPlans;
